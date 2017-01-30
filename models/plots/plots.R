9f2243512e169a7598cf0a5a87fc5320c7e2dfb9
gscontras <scontras@stanford.edu>
2017-01-30 14:58:33 -0800

    KJ's CogSci plots

--- models/plots/plots.R
+++ models/plots/plots.R
@@ -1,4 +1,4 @@
-setwd("~/Documents/git/meaningLab/kids-scope/models/plots/")
+##setwd("~/Documents/kids-scope/")
 ##setwd("~/Documents/MATLAB/RSA Functions/R Code for Graphs/kids-scope/kids-scope")
 library(ggplot2)
 library(scales)
@@ -31,53 +31,50 @@ library(scales)
 
 ## little plot
 
-q = read.csv("qud_manip9.csv",header=F)
-names(q) <- c("state","scope","QUD","p")
-q$condition = "qud"
-q$QUD = factor(q$QUD,levels=c("3","0","1","2"))
-q$QUD = factor(q$QUD,labels=c("uniform\nQUD prior","did no\nhorses succeed?","how many\nhorses succeeded?","did all\nhorses succeed?"))
-s = read.csv("scope_manip.csv",header=F)
-names(s) <- c("state","scope","QUD","p")
-s$condition = "scope"
-w = read.csv("world_manip9.csv",header=F)
-names(w) <- c("state","scope","QUD","p")
-w$condition = "world"
-w$state = factor(w$state,levels=c("4","0","1","2","3"))
-w$state = factor(w$state,labels=c("uniform\nstate prior","0 horses","1 horse","2 horses","3 horses"))
-
-
-d = rbind(s,w,q)
-
-d$x = NA
-d[d$condition=="qud",]$x = as.character(d[d$condition=="qud",]$QUD)
-d[d$condition=="scope",]$x = d[d$condition=="scope",]$scope
-d[d$condition=="world",]$x = d[d$condition=="world",]$world
-
-d$x = factor(d$x,levels=c("uniform\nQUD prior","did no\nhorses succeed?","how many\nhorses succeeded?","did all\nhorses succeed?","0.1","0.5","0.9","uniform\nstate prior","0 horses","1 horse","2 horses","3 horses"))
-d$x = factor(d$x,labels=c("uniform\nQUD prior","did no\nhorses\nsucceed?","how many\nhorses\nsucceeded?","did all\nhorses\nsucceed?","0.1","0.5","0.9","uniform\nstate prior","0 horses","1 horse","2 horses","3 horses"))
-
-d$condition = factor(d$condition,levels=c("world","qud","scope"))
-d$condition = factor(d$condition,labels=c("world state prior\nmanipulation","qud prior\nmanipulation","scope prior\nmanipulation"))
-
-lp <- ggplot(d,aes(x=as.factor(x),y=p,fill=as.factor(x))) +
-  geom_bar(stat="identity")+
-  ylab("probability of endorsing\nambiguous utterance\n") +
-  xlab("\nscope prior")+
-  #scale_fill_manual(values=c("red", "blue"))+
-  theme(axis.text.x = element_text(size=10,angle=0))+
-  scale_y_continuous(limits=c(.2,.9),oob = rescale_none)+
-  theme_bw()+
-  xlab("\nfavored parameter value")+
-  guides(fill=FALSE)+
-  facet_grid(.~condition,scales = "free_x")
-lp
+# q = read.csv("qud_manip9.csv",header=F)
+# names(q) <- c("state","scope","QUD","p")
+# q$condition = "qud"
+# q$QUD = factor(q$QUD,levels=c("3","0","1","2"))
+# q$QUD = factor(q$QUD,labels=c("uniform\nQUD prior","did no\nhorses succeed?","how many\nhorses succeeded?","did all\nhorses succeed?"))
+# s = read.csv("scope_manip.csv",header=F)
+# names(s) <- c("state","scope","QUD","p")
+# s$condition = "scope"
+# w = read.csv("world_manip9.csv",header=F)
+# names(w) <- c("state","scope","QUD","p")
+# w$condition = "world"
+# w$state = factor(w$state,levels=c("4","0","1","2","3"))
+# w$state = factor(w$state,labels=c("uniform\nstate prior","0 horses","1 horse","2 horses","3 horses"))
+# 
+# 
+# d = rbind(s,w,q)
+# 
+# d$x = NA
+# d[d$condition=="qud",]$x = as.character(d[d$condition=="qud",]$QUD)
+# d[d$condition=="scope",]$x = d[d$condition=="scope",]$scope
+# d[d$condition=="world",]$x = d[d$condition=="world",]$world
+# 
+# d$x = factor(d$x,levels=c("uniform\nQUD prior","did no\nhorses succeed?","how many\nhorses succeeded?","did all\nhorses succeed?","0.1","0.3", "0.5","0.7","0.9","uniform\nstate prior","0 horses","1 horse","2 horses","3 horses"))
+# d$x = factor(d$x,labels=c("uniform\nQUD prior","did no\nhorses\nsucceed?","how many\nhorses\nsucceeded?","did all\nhorses\nsucceed?","0.1","0.3", "0.5","0.7","0.9","uniform\nstate prior","0 horses","1 horse","2 horses","3 horses"))
+# 
+# d$condition = factor(d$condition,levels=c("world","qud","scope"))
+# d$condition = factor(d$condition,labels=c("world state prior\nmanipulation","qud prior\nmanipulation","scope prior\nmanipulation"))
+# 
+# lp <- ggplot(d,aes(x=as.factor(x),y=p,fill=as.factor(x))) +
+#   geom_bar(stat="identity")+
+#   ylab("probability of endorsing\nambiguous utterance\n") +
+#   xlab("\nscope prior")+
+#   #scale_fill_manual(values=c("red", "blue"))+
+#   theme(axis.text.x = element_text(size=10,angle=0))+
+#   scale_y_continuous(limits=c(.2,.9),oob = rescale_none)+
+#   theme_bw()+
+#   xlab("\nfavored parameter value")+
+#   guides(fill=FALSE)+
+#   facet_grid(.~condition,scales = "free_x")
+# lp
 #ggsave("little-plot.jpg",width=10.2,height=2.75)
 
 ##Super Endorsement Graph
 
-# e = read.csv("super_endorse9.csv",header=F)
-# names(e) <- c("state","scope","QUD","p")
-# 
 # se <- ggplot(e,aes(x=as.factor(x),y=p,fill=as.factor(x))) +
 #   geom_bar(stat="identity")+
 #   ylab("probability of endorsing\nambiguous utterance\n") +
@@ -88,8 +85,22 @@ lp
 #   theme_bw()+
 #   xlab("\ninverse scope prior")+
 #   guides(fill=FALSE)+
-#   facet_grid(.~condition,scales = "free_x")
 # se
-#ggsave("super_endorse.jpg",width=3,height=3)
+e = read.csv('super_endorse9.csv',header=F)
+names(e) <- c("state","scope","QUD","p") 
+
+e$scope = factor(e$scope,levels=c("0.1","0.5","0.9"))
+e$scope = factor(e$scope,labels=c("0.1","0.5","0.9"))
+# A bar graph
+se <-ggplot(data=e, aes(x=as.factor(scope), y=p,fill=as.factor(scope))) + 
+  geom_bar(stat="identity") +                        # Thinner lines
+  scale_fill_manual(values=c("limegreen", "seagreen3","royalblue1"))+      # Set legend title
+  xlab("inverse scope prior") + ylab("probability of endorsing\nambiguous utterance\n") + # Set axis labels
+  scale_y_continuous(limits=c(.3,1),oob = rescale_none)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  guides(fill=FALSE)
+se
+#ggsave("super_endorse.jpg",width=3,height=3)fill="seagreen3", colour="seagreen3"
 
 
