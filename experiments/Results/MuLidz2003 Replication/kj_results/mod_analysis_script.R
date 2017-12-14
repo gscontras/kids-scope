83a38ea372707215b8584fbe3a0ae8decfb8deee
KJSav <ksavinel@uci.edu>
2017-12-13 23:53:51 -0800

    Updated Analysis Script
    No duplicate worker ids! Removed participants who failed controls and
    calculated an Anova. Super significant results. Bayes Factor to come.

--- experiments/Results/MuLidz2003 Replication/kj_results/mod_analysis_script.R
+++ experiments/Results/MuLidz2003 Replication/kj_results/mod_analysis_script.R
@@ -20,22 +20,62 @@ unique(d$language)
 length(unique(d$workerid)) 
 head(d)
 
-## remove non-English speakers
-d = d[d$language!="Russian"&d$language!="",]
+## remove non-English speakers 
+d = d[d$language!="Cantonese"&d$language!="Chinese"&d$language!="",]
 length(unique(d$workerid)) 
 
-## determine number of observations
-table(d$condition)
+## remove participants who failed control trials
+control_matrix = subset(d, trial_num %in% c('4','6','8'))
+to_omit = subset(control_matrix, control_matrix$response > .1)
+d = subset(d, !workerid %in% c(unique(to_omit$workerid)))
+length(unique(d$workerid)) 
 
-## calculate average response by condition (hopefully)
+
+
+## determine number of observations from each condition (less from cond2)
+relevant_data = subset(d, trial_num %in% c('3','5','7','9'))
+table(relevant_data$condition)
+
+## calculate average response by condition
 agg_resp = aggregate(response~condition,mean,
-                     data=subset(d, trial_num %in% c('3','5','7','9') ))
+                       data=subset(d, trial_num %in% c('3','5','7','9') ))
+
+##for running the ANOVA
+new_thing = aggregate(relevant_data$response,
+                      by = list(relevant_data$workerid, relevant_data$condition),
+                      FUN = 'mean')
+
+colnames(new_thing) = c("WorkerID","Condition","AverageResponse")
+new_thing = new_thing[order(new_thing$WorkerID),]
+head(new_thing)
+
+new_thing.aov = with(new_thing, 
+                     aov(AverageResponse ~ Condition + Error(WorkerID/Condition)))
+
+summary(new_thing.aov)
+
+##If the website I got this from is correct, we have super significance!
+
+
+
+
+## average score on control trials (most people correctly rejected)
+agg_controls = aggregate(response~condition,mean,
+                     data=subset(d, trial_num %in% c('4','6','8') ))
+
+###Checking for duplicate Ids - there are none
+ids = read.csv('WorkerIDs.csv')
+length(unique(ids$Actual.ID))
+n_occur <- data.frame(table(ids$Actual.ID))
+
 
 ####Condition types
 #cond1 = two
 #cond2 = two contrast
 #cond3 = four
 #cond4 = four contrast
+
+
 ## calculate average subjectivity by class
 ## agg_class = aggregate(response~class,data=d,mean)
 
